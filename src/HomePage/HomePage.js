523aa66f9a2b4fd3c01699e0e0d4ecc257fbcb81
unknown <sankalprane1998@gmail.com>
2023-01-29 20:19:47 -0600

    logic implemented and bfs completed

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -4,7 +4,7 @@ import './HomePage.css';
 
 export default function HomePage() {
 
-  const [configuration, setConfiguration] = useState([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]);
+  const [configuration, setConfiguration] = useState([[5, 1, 2, 3], [9, 6, 7, 4], [13, 10, 11, 8], [0, 14, 15, 12]]);
   const Grid = [];
   const dir_row = [1, -1, 0, 0];
   const dir_col = [0, 0, 1, -1];
@@ -52,20 +52,77 @@ export default function HomePage() {
     return newArray;
   }
 
-  function swapNumbers() {
-    console.log('working??');
-    setConfiguration((oldConf) => {
-      const newConf = deepCopyArray(oldConf)
-      newConf[3][3] = Math.floor(Math.random() * 100) + 1;
-      return newConf;
-    })
-    console.log(configuration.toString());
+  function getCoordinatesOfBlank(conf) {
+    let x, y;
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (conf[i][j] === 0) {
+          x = i;
+          y = j;
+        }
+      }
+    }
+    return [x, y];
   }
+
+  function nextMove(conf, move) {
+    let [oldX, oldY] = getCoordinatesOfBlank(conf);
+    let newX = oldX, newY = oldY;
+    if (move === 'D') {
+      newX = oldX + 1;
+    } else if (move === 'U') {
+      newX = oldX - 1;
+    } else if (move === 'L') {
+      newY = oldY - 1;
+    } else {
+      newY = oldY + 1;
+    }
+    return [oldX, oldY, newX, newY];
+  }
+
+  function solve(path) {
+    for (let i =0; i < path.length; i++) {
+    let move = path[i]
+    setTimeout(() => {
+      console.log(move);
+      setConfiguration((oldConf) => {
+        let [oldX, oldY, newX, newY] = nextMove(oldConf, move);
+        console.log(oldX, oldY, newX, newY);
+        const newConf = deepCopyArray(oldConf);
+        let temp = newConf[newX][newY];
+        newConf[newX][newY] = newConf[oldX][oldY];
+        newConf[oldX][oldY] = temp;
+        return newConf;
+      })
+    }, 1000 * i);
+
+    }
+  }
+
+  function solvePuzzle() {
+    console.log('working??');
+    postData().then((res) => {
+      console.log(res);
+      solve(res.path);
+    });
+  }
+
+  async function postData(url, data = { start: configuration }) {
+    const response = await fetch('http://localhost:4000/bfs/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    return response.json(); 
+  }
+  
 
   return (
     <>
       {Grid}
-      <button onClick={swapNumbers}>Click ME!</button>
+      <button onClick={solvePuzzle}>Click ME!</button>
     </>
   )
 }
