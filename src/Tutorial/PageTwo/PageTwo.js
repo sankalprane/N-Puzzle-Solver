5bb3baf2fcbe3395fde39e39175333a71359df52
sankalprane <sankalprane1998@gmail.com>
2023-02-15 21:14:31 -0600

    refactored Tutorial to use the same modal

--- src/Tutorial/PageTwo/PageTwo.js
+++ src/Tutorial/PageTwo/PageTwo.js
@@ -1,49 +1,16 @@
 import React, { useEffect } from 'react';
 import Puzzle from '../../Puzzle/Puzzle';
-import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    Button,
-    useDisclosure
-} from '@chakra-ui/react'
 
-export default function PageOne({previousPage}) {
+export default function PageOne({ previousPage }) {
 
-    const { isOpen, onOpen, onClose } = useDisclosure()
     const conf = [[1, 2, 3], [4, 5, 6], [7, 8, 0]];
-
-    useEffect(() => {
-        onOpen();
-    }, []);
 
     return (
         <>
-            <Button onClick={onOpen}>Open Modal</Button>
-
-            <Modal closeOnOverlayClick={false} size='xl' isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>TUTORIAL</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody pb={6}>
-
-                        <div className='tutorial-text-1'>The goal of the puzzle is to arrange the numbers in ascending order as shown below</div>
-                        <div className='tutorial-modal-body'>
-                            <Puzzle grid={conf} />
-                        </div>
-                    </ModalBody>
-
-                    <ModalFooter>
-                        <Button colorScheme='teal' onClick={previousPage}>Back</Button>
-                        <Button colorScheme='teal' onClick={onClose}>Cancel</Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
+            <div className='tutorial-text-1'>The goal of the puzzle is to arrange the numbers in ascending order as shown below</div>
+            <div className='tutorial-modal-body'>
+                <Puzzle grid={conf} />
+            </div>
         </>
     )
 }
--- src/Tutorial/Tutorial.js
+++ src/Tutorial/Tutorial.js
@@ -1,11 +1,27 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './Tutorial.css'
 import PageOne from './PageOne/PageOne';
 import PageTwo from './PageTwo/PageTwo';
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+    useDisclosure
+} from '@chakra-ui/react'
 
 export default function Tutorial() {
 
+    const { isOpen, onOpen, onClose } = useDisclosure()
     const [page, setPage] = useState(1);
+
+    useEffect(() => {
+        onOpen();
+    }, []);
 
     function nextPage() {
         setPage((pageNumber) => {
@@ -17,7 +33,7 @@ export default function Tutorial() {
 
     function previousPage() {
         setPage((pageNumber) => {
-            pageNumber--;
+                pageNumber--;
             console.log(pageNumber);
             return pageNumber;
         })
@@ -25,12 +41,31 @@ export default function Tutorial() {
 
     let displayPage;
     if (page === 1) {
-        displayPage = <PageOne nextPage={nextPage}></PageOne>
+        displayPage = <PageOne></PageOne>
     } else {
-        displayPage = <PageTwo previousPage={previousPage}></PageTwo>
+        displayPage = <PageTwo></PageTwo>
     }
 
     return (
-        displayPage
+            <>
+            <Button onClick={onOpen}>Open Modal</Button>
+    
+            <Modal closeOnOverlayClick={false} size='xl' isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>TUTORIAL</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody pb={6}>
+                        {displayPage}
+                    </ModalBody>
+    
+                    <ModalFooter>
+                        <Button colorScheme='teal' onClick={previousPage}>Back</Button>
+                        <Button colorScheme='teal' onClick={nextPage}>Next</Button>
+                        <Button colorScheme='teal' onClick={onClose}>Cancel</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
     )
 }
